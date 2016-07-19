9f5e8706d6b2de85fa9e9f378f0b7fafecb29aa6
demon <noparkinghere@foxmail.com>
2016-07-19 11:43:33 +0800

    update awesome configuration

--- rc.lua
+++ rc.lua
@@ -721,7 +721,7 @@ end
 clientbuttons = awful.util.table.join(
     awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
     awful.button({ modkey }, 1, awful.mouse.client.move),
-    awful.button({ modkey }, 3, awful.mouse.client.resize))
+    awful.button({ altkey }, 1, awful.mouse.client.resize))
 
 -- Set keys
 root.keys(globalkeys)
