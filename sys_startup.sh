062db8f68d4a32715637077a77aa4ab01a0aece0
demon <noparkinghere@foxmail.com>
2016-10-30 21:38:31 +0800

    update awesome configuration

--- sys_startup.sh
+++ sys_startup.sh
@@ -23,7 +23,7 @@ sudo ip route add 123.206.229.202 via 192.168.18.1 dev eno1
 # 挂在硬盘的独立分区
 sudo mount /dev/sda5 /home/demon/Space/
 # 挂载硬盘到当前用户目录下的Download目录中
-sudo mount /dev/sda6 ~/Extern/
+sudo mount /dev/sda6 /home/demon/Extern/
 
 # fcitx deamon
 sudo fcitx -d
