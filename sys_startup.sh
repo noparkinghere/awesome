37af4d51ebcd7cb27aa0817e56b79c2d8e840e9b
demon <noparkinghere@foxmail.com>
2016-11-06 18:42:03 +0800

    update awesome configuration

--- sys_startup.sh
+++ sys_startup.sh
@@ -1,6 +1,5 @@
-#!/bin/sh -e
-#
-# rc.local
+
+cal
 #
 # This script is executed at the end of each multiuser runlevel.
 # Make sure that the script will "exit 0" on success or any other
@@ -11,17 +10,21 @@
 #
 # By default this script does nothing.
 
-# 挂在硬盘的独立分区
-# sudo mount /dev/sda5 /home/demon/Media
-# sudo mount /dev/sda6 /home/demon/Learn
 
 # github的访问不经过VPN，直接走网卡
-# sudo ip route add 192.30.252.0/22 via 192.168.18.1 dev eno1
+sudo ip route add 192.30.252.0/22 via 192.168.18.1 dev eno1
+# 腾讯云服务器ssh过滤
+sudo ip route add 123.206.229.202 via 192.168.18.1 dev eno1
+
 # 键盘caps lock和右侧ctrl进行交换
 # xmodmap /home/demon/.xmodmap 2>/dev/null
 
-# gnome下调用网络管理控件
-# nm-applet
+# 挂载硬盘分区
+sudo mount /dev/sda5 /home/demon/Media
+sudo mount /dev/sda6 /home/demon/Learn
 
+
+# fcitx deamon
+sudo fcitx -d
 
 exit 0
